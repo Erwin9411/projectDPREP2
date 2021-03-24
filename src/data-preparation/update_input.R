18df08896f75600a09faae13d6cd74c59a6e0427
Bas Dekkers <bhcjdekkers@home.nl>
2021-03-24 17:05:06 +0100

    adding amazon cleaning

--- src/data-preparation/update_input.R
+++ src/data-preparation/update_input.R
@@ -73,12 +73,76 @@ bol_dataset$price = as.numeric(as.character(bol_dataset$price))
 
 
 ##################AMAZON######################
+library(tibble)
+library(readr)
+library(dplyr)
+data_amazon <- read.csv("../../data/amazon/data_amazon_phones_20210323.csv", header = FALSE, sep = " ", fileEncoding="utf8", flush = TRUE)
 
-colnames(data_amazon) <- #c("title",
-                           #"price",
-                           #"tech",
-                           #"other",
-                           #"extra")
+colnames(data_amazon) <- c("title_name",
+                           "title",
+                           "brand_name",
+                           "brand",
+                           "price_name",
+                           "price",
+                           "star_rating_name",
+                           "star_rating",
+                           "review_count_name",
+                           "review_count",
+                           "asin_name",
+                           "asin",
+                           "date_name",
+                           "date")
+
+#deleting uncessary columns
+data_amazon_column <- subset(data_amazon, select = -c(title_name, brand_name, review_count_name, price_name, star_rating_name, asin_name, date_name))
+
+#removing last commas
+data_amazon_column$title = substr(data_amazon_column$title,1,nchar(data_amazon_column$title)-1)
+data_amazon_column$brand = substr(data_amazon_column$brand,1,nchar(data_amazon_column$brand)-1)
+data_amazon_column$price = substr(data_amazon_column$price,1,nchar(data_amazon_column$price)-1)
+data_amazon_column$star_rating = substr(data_amazon_column$star_rating,1,nchar(data_amazon_column$star_rating)-1)
+data_amazon_column$review_count = substr(data_amazon_column$review_count,1,nchar(data_amazon_column$review_count)-1)
+data_amazon_column$asin = substr(data_amazon_column$asin,1,nchar(data_amazon_column$asin)-1)
+data_amazon_column$date = substr(data_amazon_column$date,1,nchar(data_amazon_column$date)-1)
+
+#delete incomplete rows
+data_amazon_column <- data_amazon_column %>% filter(grepl("\\u20ac", data_amazon_column$price))
+data_amazon_column <- data_amazon_column %>% filter(grepl(" beoordelingen", data_amazon_column$review_count))
+data_amazon_column <- data_amazon_column %>% filter(grepl(" van 5 sterren", data_amazon_column$star_rating))
+
+#remove unnecessary information in cells
+data_amazon_column$price <- gsub("\\u20ac", "", data_amazon_column$price)
+data_amazon_column$price <- substr(data_amazon_column$price,2,nchar(data_amazon_column$price)-1)
+data_amazon_column$review_count <- gsub(" beoordelingen", "", data_amazon_column$review_count)
+data_amazon_column$star_rating <- gsub(" van 5 sterren", "", data_amazon_column$star_rating)
+
+#deleting duplicates
+data_amazon_column <- data_amazon_column %>% filter(!duplicated(data_amazon_column))
+
+#improving data quality
+data_amazon_column$brand <- gsub("De Nokia Store openen", "Nokia", data_amazon_column$brand)
+
+#transforming all to lower capitals
+data_amazon_column$brand <- tolower(data_amazon_column$brand)
+
+#transforming price and starts to numeric
+data_amazon_column$price <- gsub("[.]", "", data_amazon_column$price)
+data_amazon_column$price <- gsub(",", ".", data_amazon_column$price)
+data_amazon_column$price <- as.numeric(data_amazon_column$price)
+
+data_amazon_column$star_rating <- gsub(",", ".", data_amazon_column$star_rating)
+data_amazon_column$star_rating <- as.numeric(data_amazon_column$star_rating)
+
+#transforming review_count to integer
+data_amazon_column$review_count <- gsub("[.]", "", data_amazon_column$review_count)
+data_amazon_column$review_count <- as.integer(data_amazon_column$review_count)
+
+#transforming date to date
+data_amazon_column$date <- as.Date(data_amazon_column$date,format="%Y-%m-%d")
+
+#data.table
+#fread() replaces read.table
+#data_bol = data.frame(fread(...))
 
 write.csv(data_bol,file="../../gen/data-preparation/input/data_bol.csv", row.names = FALSE)
 write.csv(data_amazon,file="../../gen/data-preparation/input/data_amazon.csv", row.names = FALSE)
